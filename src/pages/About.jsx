b78aa12a30ffa656bc916a95c0c4464e7fef7ff3
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-13 13:25:50 +0530

    1

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="container px-12">
+        <h1 className="text-6xl text-white mb-4">Github Finder</h1>
+        <p className="mb-4 text-xl font-light">A React app to search GitHub profiles and see profile details. This project explores the use of React Routers, Tailwind, working with API and lot more</p>
+        <p className="text-lg text-gray-200">Version: <span className="font-bold">1.0.0</span></p>
+        <p className="text-lg text-gray-200">Layout By: <span className="font-bold">Hassib Moddasser</span></p>
+    </div>
+  )
+}
+
+export default About
