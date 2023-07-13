33125434d7536954d0f82ed4c5c0ee58c232f3c8
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-13 13:25:32 +0530

    5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
-    <div >
-      <h1 >{React.version}</h1>
-    </div>
+    <Router >
+      <div className="flex flex-col justify-between h-screen bg-gray-600">
+        <Navbar />
+        <main className="mx-auto container px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
