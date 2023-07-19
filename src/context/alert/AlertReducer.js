b033cb6b605ae17d38cc4c9aaa08714ef3266602
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-19 12:01:20 +0530

    5

--- src/context/alert/AlertReducer.js
+++ src/context/alert/AlertReducer.js
@@ -1,4 +1,4 @@
-const AlertReducer = (state, action) => {
+const alertReducer = (state, action) => {
     switch (action.type) {
         case 'SET_ALERT':
             return action.payload
@@ -9,4 +9,4 @@ const AlertReducer = (state, action) => {
     }
 }
 
-export default AlertReducer;
+export default alertReducer;
