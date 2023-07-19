ef178020ee8a8ab545638ae346d69265ba753d2e
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-19 12:18:46 +0530

    2

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
@@ -40,6 +41,28 @@ export const GithubProvider = ({children}) => {
         }
     }
 
+    //get user profile
+    const getUser = async (login) => {
+        setLoading()
+
+        const response = await fetch (`${GITHUB_URL}/users/${login}`,
+        // {
+        //     headers: {
+        //         Authorization: `token ${GITHUB_TOKEN}`
+        //     }
+        // }
+        )
+        if (response.ok) {
+            const {data} = await response.json();
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        } else {
+            window.location = '/notfound'
+        }
+    }
+
     const setLoading = () => {
         dispatch({
             type: 'SET_LOADING'
@@ -50,7 +73,7 @@ export const GithubProvider = ({children}) => {
             type: 'CLEAR_USERS'
         })
     }
-    return <GithubContext.Provider value={{users: state.users, loading: state.loading, searchUsers, clearUsers}}>
+    return <GithubContext.Provider value={{users: state.users, user: state.user, loading: state.loading, searchUsers, clearUsers, getUser}}>
         {children}
     </GithubContext.Provider>
 }
