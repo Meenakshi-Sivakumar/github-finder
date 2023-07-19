7df5c408b88ae3231ea21c4dbf70454adc25c365
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-07-19 12:18:48 +0530

    3

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect, useContext } from "react"
+import { useParams } from "react-router-dom"
+import GithubContext from "../context/github/GithubContext"
+
+const params = useParams()
 
 function User() {
+    const {getUser, user} = useContext(GithubContext)
+
+    useEffect(() => {
+        getUser(params.login)
+        getUserRepos(params.login)
+    }, [])
   return (
     <div>
       user
